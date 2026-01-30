d073de20be4448b7117b19369d969ea54dc5f43b
Phellipe <phellipelisboa.dev@gmail.com>
2026-01-30 12:12:02 -0300

    feat: add custom queries in ProfessionalRepository

--- backend/src/main/java/com/phellipe/workoutplanner/backend/domain/repository/ProfessionalRepository.java
+++ backend/src/main/java/com/phellipe/workoutplanner/backend/domain/repository/ProfessionalRepository.java
@@ -4,6 +4,16 @@ import com.phellipe.workoutplanner.backend.domain.entity.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
+
+    Boolean existsByEmail(String email);
+
+    Optional<Professional> findByEmail(String email);
+
+    List<Professional> findByActiveTrue();
+
 }
