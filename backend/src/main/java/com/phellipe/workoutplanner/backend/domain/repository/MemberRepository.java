9b6c91926f8b6657c45f1373b1af7af1a966d9b8
Phellipe <phellipelisboa.dev@gmail.com>
2026-01-30 10:23:00 -0300

    feat: add custom queries in MemberRepository

--- backend/src/main/java/com/phellipe/workoutplanner/backend/domain/repository/MemberRepository.java
+++ backend/src/main/java/com/phellipe/workoutplanner/backend/domain/repository/MemberRepository.java
@@ -4,6 +4,16 @@ import com.phellipe.workoutplanner.backend.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByPublicCode(String publicCode);
+
+    boolean existsByPublicCode(String publicCode);
+
+    List<Member> findByActiveTrue();
+
 }
